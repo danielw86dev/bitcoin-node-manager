8768f1b042f52ddaaeb9acec5cb16fa8191843ac
danielw86dev <67072678+danielw86dev@users.noreply.github.com>
2024-04-14 15:09:36 +0200

    doge: Re-added mempool page since network less congested

--- src/Content.php
+++ src/Content.php
@@ -222,8 +222,10 @@ function createRulesContent($editID = NULL){
 function createMempoolContent(){
 	global $bitcoind;
 
+	if (Config::BLOCKCHAIN_NETWORK == "dogecoin"){
 	$content['txs'] = $bitcoind->getrawmempool(TRUE);
 	$content['txs'] = array_slice($content['txs'], 0, CONFIG::DISPLAY_TXS);
+	}
 	$content['node'] = new Node();
 
 	return $content;
